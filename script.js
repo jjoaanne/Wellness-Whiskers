e40e1b9acba0300380d7ec89f1412f9004c087bf
Joanne <Joannedoan111@gmail.com>
2023-04-22 16:16:53 -0400

    z

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 var mood = [
-    'YAY! It is great that you are feeling happy!', 
-    'Keep thinking postively and everything will be okay!', 
+    'YAY! It is great that you are feeling happy!',
+    'Keep thinking postively and everything will be okay!',
 ]
 var reason = [
-    'Remember that communication and honesty is key! If problems occur, try to communicate and compromise so that the both of you could agree on something.', 
-    'Remember that you can always talk to your teachers, create a schedule, study with friends and most of all, have a postive mindset and take care of yourself mentaly and physically!', 
+    'Remember that communication and honesty is key! If problems occur, try to communicate and compromise so that the both of you could agree on something.',
+    'Remember that you can always talk to your teachers, create a schedule, study with friends and most of all, have a postive mindset and take care of yourself mentaly and physically!',
     'Remember that you can you can always talk to your boss or co-workers, stay punctual, organized, communicate well, and take care of yourself!',
 ]
 
 function handleClick() {
     const radioButtons1 = document.querySelectorAll('input[name="mood-selection"]');
-    var selectedValue; 
+    var selectedValue;
     const radioButtons2 = document.querySelectorAll('input[name="reason-selection"]');
     var selectedValue2;
 
     for (const rb of radioButtons1) {
         if (rb.checked) {
-        selectedValue = rb.value;
-        break; 
+            selectedValue = rb.value;
+            break;
         }
     }
     for (const rb of radioButtons2) {
         if (rb.checked) {
-        selectedValue2 = rb.value;
-        break;
+            selectedValue2 = rb.value;
+            break;
         }
     }
 
-    if(selectedValue && selectedValue2) {
+    if (selectedValue && selectedValue2) {
         document.getElementById('response').innerHTML = mood[selectedValue] + " " + reason[selectedValue2];
-    } 
+    }
     else if (selectedValue === undefined && selectedValue2 === undefined) {
         alert("Please Select a Mood and Reason!");
-    } 
+    }
     else if (selectedValue === undefined) {
         alert("Please Select a Mood!");
-    } 
+    }
     else if (selectedValue2 === undefined) {
         alert("Please Select a Reason!");
-    } 
+    }
 }
 
 function clearPressed() {
